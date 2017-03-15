5eb6b4300c5d223e1c2f5b41b30ffa7f7b2b86fa
Sebastián <palmabatun.s@gmail.com>
2017-03-15 18:24:03 +0100

    cambios en master

--- Lib.java
+++ Lib.java
@@ -6,7 +6,7 @@
 public class Lib {
 
     public void metodoAlumno1(){
-        System.out.println("metodo 1");
+        metodoAlumno2();
     }
 
     public void metodoAlumno2(){
@@ -14,7 +14,7 @@ public class Lib {
     }
 
     public void metodoComunitario(){
-        System.out.println("Aqui escribimos todos");
+        System.out.println("cambio en master");
     }
     public  void  metodo3 (){
 	System.out.println("metodo 3");
